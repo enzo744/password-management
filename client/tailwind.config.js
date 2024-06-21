2efb58647ba96a14ba7eef57b40792cb2b5ac5b5
EnzoPa70 <enzo.angular50@gmail.com>
2024-06-21 18:42:09 +0200

    install react js and tailwind css and create the first template

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx,mjs,mjs.map}",
+  ],
+  theme: {
+    extend: {},
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [
+    // require("flowbite/plugin"),
+    // require("tailwind-scrollbar"),
+  ],
+};
+
