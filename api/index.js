cdf022ee87124b411c733a9efc2e1d89476ce5d6
enzo744 <vnalbone0@gmail.com>
2024-06-23 19:43:22 +0200

    Connessione al database Mongo db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => {
+    console.log("Il Database MongoDB e' connesso");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3002, () =>{
-    console.log("Server is running on port 3002!!");
+app.listen(3002, () => {
+  console.log("Server is running on port 3002!!");
 });
-
