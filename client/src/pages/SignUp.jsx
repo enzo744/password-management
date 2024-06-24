b341624477610d2dc892e3f3626f1a3319f461b1
enzo744 <vnalbone0@gmail.com>
2024-06-24 18:48:04 +0200

    add functionality sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,44 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { Link, useNavigate } from "react-router-dom";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { useState } from "react";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
 
   const [open, setOpen] = useState(false);
   const toggle = () => {
@@ -19,43 +54,71 @@ export default function SignUp() {
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-yellow-300 rounded-lg text-white">
               Password App
             </span>
-            
           </Link>
           <p className="text-sm mt-5">
-            Progetto dimostrativo, finalizzato alla gestione di vari campi personali.
+            Progetto dimostrativo, finalizzato alla gestione di vari campi
+            personali.
           </p>
         </div>
         {/* right */}
 
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div className="">
               <Label value="Il tuo Username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
               <Label value="La tua Email" />
-              <TextInput type="text" placeholder="Email" id="email" />
+              <TextInput
+                type="text"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div className="relative text-2xl">
-            <Label value="La tua Password***" />
+              <Label value="La tua Password***" />
               <TextInput
                 className="w-full"
                 type={open === false ? "password" : "text"}
                 placeholder="Password"
                 id="password"
-                // onChange={handleChange}
+                onChange={handleChange}
               />
               <div className="absolute top-10 right-3">
                 {open === false ? (
-                  <AiFillEye onClick={toggle} className="cursor-pointer"/>
+                  <AiFillEye onClick={toggle} className="cursor-pointer" />
                 ) : (
-                  <AiFillEyeInvisible onClick={toggle} className="cursor-pointer"/>
+                  <AiFillEyeInvisible
+                    onClick={toggle}
+                    className="cursor-pointer"
+                  />
                 )}
               </div>
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Registrati
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  {/* <Spinner size="sm" /> */}
+                  <Spinner
+                    color="success"
+                    aria-label="Success spinner example"
+                  />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Registrati"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-3">
@@ -64,6 +127,11 @@ export default function SignUp() {
               Accedi
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
